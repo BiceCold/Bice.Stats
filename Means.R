ffe91f012b6ea597e45e969efe1b5fef557e0d9f
BiceCold <bice.jeremy@outlook.com>
2017-09-20 21:11:52 -0700

    Update and rename Bice.R to Means.R
    Various means used in statistics

--- Means.R
+++ Means.R
@@ -12,3 +12,13 @@ Bice.NaturalAntiLog<-function(x) {
   exp(meanlogy)
   geometric(x)
 }
+
+#function for harmonic mean
+Bice.HarmonicMean<-function(x) {
+  length(x)/sum(1/x)
+}
+
+#function for arithmetic mean
+Bice.ArithmeticMean<-function(x) {
+  sum(x)/length(x)
+}
